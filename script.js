f8d726ddd20f95bd35eda075a2c0db8c156aac86
Rogerio Gonzaga <rogeriogonzaga84@gmail.com>
2026-09-24 17:59:27 -0300

    Atualização 17:59

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert('Ola mundo')
+// Este e um comentario parte A do programa  
+
+/* Este e um bloco de comentario 
+para os proximos Devs
+*/
+
+console.log('Rogerio')
+console.warn('Erro 300')
+console.error('Erro 504')
+
+// Este e um comentario parte B do programa 
+
+console.group('App')
+console.log('Rogerio')
+console.warn('Erro 300')
+console.error('Erro 504')
+console.timeStamp('Data e Hora')
+console.groupEnd()
